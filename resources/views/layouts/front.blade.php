7ff1c5eb9a5847f3581ada733aa753d0bcc51157
Shankar <shankar@codelantic.com>
2024-10-03 13:15:25 +0530

    favicon update

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -13,7 +13,10 @@
 
     <!-- favicons
     ================================================== -->
-    <link rel="icon" type="image/png"  href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.16x16.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
 
     <!-- mobile specific metas
     ================================================== -->
